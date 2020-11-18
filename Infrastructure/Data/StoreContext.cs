e83f74ac618277ccd25d6277aa9afcc562217452
Jayesh Patel <jaypatel4k@gmail.com>
2020-11-18 09:08:32 +0530

    Commit section 3

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,7 +9,15 @@ namespace Infrastructure.Data
         public StoreContext( DbContextOptions<StoreContext> options) : base(options)
         {
         }
-
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+
+        //follwong method is used to configure 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
