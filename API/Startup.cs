13f3333e0c90511bbe6275455088085062b87990
Jayesh Patel <jaypatel4k@gmail.com>
2020-11-30 09:25:10 +0530

    End of Section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -41,6 +41,12 @@ namespace API
             services.AddDbContext<StoreContext>(X => X.UseSqlServer(_config.GetConnectionString("DefaultConnection")));
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+
+            services.AddCors(opt=>{
+                opt.AddPolicy("CorsPolicy",policy=> {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
             
             
         }
@@ -64,9 +70,13 @@ namespace API
 
             app.UseStaticFiles();
 
+            app.UseCors("CorsPolicy");
+
             app.UseAuthorization();
 
             app.UseSwaggerDocumentation();            
+
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
