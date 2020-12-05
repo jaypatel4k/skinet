298531e26ca6ba064290d6314351b09aa00ed1d8
Jayesh Patel <jaypatel4k@gmail.com>
2020-12-05 13:10:48 +0530

    End of Section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { error } from 'protractor';
+import { Observable } from 'rxjs';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+  products: IProduct[] = [];
+  constructor(private http: HttpClient)
+  {
+
+  }
+  ngOnInit(): void {
+    this.getCandies().subscribe(
+      (prod) => {
+        this.products = prod.data;
+      },
+      (err) =>
+      { console.error(err);
+    });
+  }
+    getCandies(): Observable<IPagination<IProduct[]>>{
+      return this.http.get<IPagination<IProduct[]>>('https://localhost:5001/api/products?pageSize=50');
+    }
 }
---
+++ client/src/app/models/pagination.ts
@@ -0,0 +1,8 @@
+  import { IProduct } from './product';
+
+  export interface IPagination<T> {
+    pageIndex: number;
+    pageSize: number;
+    count: number;
+    data: T;
+  }
